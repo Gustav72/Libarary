88ae62b1084d85e3962f575bc4eabd3169cf85c3
Gustav72 <wolfgangkirstein7@gmail.com>
2023-03-26 11:48:35 -0400

    add comment

--- script.js
+++ script.js
@@ -1,106 +1,96 @@
+//Array for books
 let myLibrary = [];
 let i = 0;
-const form = document.querySelector('form');
+const form = document.querySelector("form");
 
 function Book() {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 }
 
-
 function switchStatus(j) {
-    let status = document.getElementById('finish' + j)
-    if (status.innerText == 'yes') {
-        status.innerText = 'no';
-        status.style.color = "red";
-        myLibrary[j - 1].read = 'no'
-    }
-    else {
-        status.innerText = 'yes';
-        status.style.color = "green";
-        myLibrary[j - 1].read = 'yes'
-    }
+  let status = document.getElementById("finish" + j);
+  if (status.innerText == "yes") {
+    status.innerText = "no";
+    status.style.color = "red";
+    myLibrary[j - 1].read = "no";
+  } else {
+    status.innerText = "yes";
+    status.style.color = "green";
+    myLibrary[j - 1].read = "yes";
+  }
 }
 
 function remove(row) {
-   document.getElementById(row).remove(); //delete from DOM
-    delete myLibrary[row-1] //delete from array
-
+  document.getElementById(row).remove(); //delete from DOM
+  delete myLibrary[row - 1]; //delete from array
 }
 
-
 function addBookToLibrary() {
+  i++;
 
-    i++;
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+  });
 
-    form.addEventListener('submit', 
-    function(event) {
-        event.preventDefault()
-    });
+  let newBook = new Book();
+  let title = document.getElementById("title").value;
+  let author = document.getElementById("author").value;
+  let pages = document.getElementById("pages").value;
+  let read = document.querySelector('input[name="read"]:checked').value;
 
-    let newBook = new Book();
-    let title = document.getElementById('title').value
-    let author = document.getElementById('author').value
-    let pages = document.getElementById('pages').value
-    let read = document.querySelector('input[name="read"]:checked').value;
+  if (title == "" || author == "" || pages == "" || read == "") return;
 
-    if(title == '' || author == '' || pages == '' || read == '' ) return;
+  newBook.title = title;
+  newBook.author = author;
+  newBook.pages = pages;
+  newBook.read = read;
 
-    newBook.title = title;
-    newBook.author = author;
-    newBook.pages = pages;
-    newBook.read = read;
+  myLibrary.push(newBook);
 
-    myLibrary.push(newBook);
+  const table = document.querySelector("tbody");
 
+  const row = document.createElement("tr");
 
-    const table = document.querySelector('tbody');
+  let cell1 = document.createElement("td");
+  let cell2 = document.createElement("td");
+  let cell3 = document.createElement("td");
+  let cell4 = document.createElement("td");
+  let cell5 = document.createElement("td");
 
-    const row = document.createElement('tr')
+  row.setAttribute("id", i);
 
-    let cell1 = document.createElement('td');
-    let cell2 = document.createElement('td');
-    let cell3 = document.createElement('td');
-    let cell4 = document.createElement('td');
-    let cell5 = document.createElement('td');
+  cell5.setAttribute("onclick", "remove(" + i + ")");
 
-    row.setAttribute('id', i);
+  cell4.setAttribute("onclick", "switchStatus(" + i + ")");
+  cell4.setAttribute("id", "finish" + i);
 
-    cell5.setAttribute('onclick', 'remove(' + i + ')');
+  cell1.innerText = newBook.title;
+  cell2.innerText = newBook.author;
+  cell3.innerText = newBook.pages;
+  cell4.innerText = newBook.read;
+  cell5.innerText = "✖";
 
-    cell4.setAttribute('onclick', 'switchStatus(' + i + ')');
-    cell4.setAttribute('id', 'finish' + i);
+  row.appendChild(cell1);
+  row.appendChild(cell2);
+  row.appendChild(cell3);
+  row.appendChild(cell4);
+  row.appendChild(cell5);
 
+  table.appendChild(row);
 
+  newBook.title = document.getElementById("title").value;
+  newBook.author = document.getElementById("author").value;
+  newBook.pages = document.getElementById("pages").value;
+  newBook.read = document.getElementById("read").value;
 
-    cell1.innerText = newBook.title;
-    cell2.innerText = newBook.author;
-    cell3.innerText = newBook.pages;
-    cell4.innerText = newBook.read;
-    cell5.innerText = '✖';
+  if (read == "yes")
+    document.getElementById("finish" + i).style.color = "green";
+  else document.getElementById("finish" + i).style.color = "red";
 
-    row.appendChild(cell1);
-    row.appendChild(cell2);
-    row.appendChild(cell3);
-    row.appendChild(cell4);
-    row.appendChild(cell5);
+  form.reset();
 
-    table.appendChild(row);
-    
-
-    newBook.title = document.getElementById('title').value
-    newBook.author = document.getElementById('author').value
-    newBook.pages = document.getElementById('pages').value
-    newBook.read = document.getElementById('read').value
-
-    if (read == 'yes') document.getElementById('finish' + i).style.color = "green";
-    else document.getElementById('finish' + i).style.color = "red";
-
-    form.reset();
-    
-    console.log(read);
-
+  console.log(read);
 }
